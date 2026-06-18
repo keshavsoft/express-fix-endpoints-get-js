3d319c30dde80743e24ac0a46d424389fe3d7563
keshavsoft <nknnkumar@yahoo.com>
2026-06-18 10:37:45 +0530

    to test

--- bin/v2/UpdateJs/index.js
+++ bin/v2/UpdateJs/index.js
@@ -1,22 +1,23 @@
 import fixAnyJs from "express-fix-any-js";
 import checkLines from "./checkLines.json" with {type: "json"};
 
-const alterLines = ({ inActionName }) => {
+const alterLines = ({ inActionName, inFolderName }) => {
     let localCheckLines = checkLines;
 
-    localCheckLines.importLines.toInsertLine = localCheckLines.importLines.toInsertLine.replaceAll("${endpoint}", inActionName);
-    localCheckLines.importLines.duplicationCheck = localCheckLines.importLines.duplicationCheck.replaceAll("${endpoint}", inActionName).replaceAll("'", '"');
+    localCheckLines.importLines.toInsertLine = localCheckLines.importLines.toInsertLine.replaceAll("${folderName}", inFolderName);
+    localCheckLines.importLines.duplicationCheck = localCheckLines.importLines.duplicationCheck.replaceAll("${folderName}", inFolderName).replaceAll("'", '"');
 
     localCheckLines.useLines.toInsertLine = localCheckLines.useLines.toInsertLine.replaceAll("${endpoint}", inActionName);
+    localCheckLines.useLines.toInsertLine = localCheckLines.useLines.toInsertLine.replaceAll("${folderName}", inFolderName);
+
     localCheckLines.useLines.duplicationCheck = localCheckLines.useLines.duplicationCheck.replaceAll("${endpoint}", inActionName).replaceAll("'", '"');
 
     return localCheckLines;
 };
 
-const startFunc = ({ inJsFilePath, inActionName, showLog = false }) => {
+const startFunc = ({ inJsFilePath, inActionName, inFolderName, showLog = false }) => {
 
     const localCheckLines = alterLines({ inActionName });
-    console.log("localCheckLines : ", inJsFilePath);
 
     fixAnyJs({
         showLog,
